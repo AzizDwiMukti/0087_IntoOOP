3d70deadbb256d8820387bd15fd49f2cb4e0dd6d
ASUS <ASUS@AZIZDWIMUKTI>
2024-04-22 14:56:31 +0700

    memasukan class matakuliah

--- 0087_IntoOOP/0087_IntoOOP.cpp
+++ 0087_IntoOOP/0087_IntoOOP.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa{
+class Mahasiswa {
 public: // akses modifier
 	string nama;
 	int umur;
 	string jurusan;
 
 	void output() {
-
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
 	}
-}
+};
+
+
+
+class Matakuliah {
+
+};
